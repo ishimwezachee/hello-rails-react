ff9306550b10755432963e627a9f3515cbe29989
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-21 16:46:06 +0200

    seed some data

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -3,7 +3,7 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
