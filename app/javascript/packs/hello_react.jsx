964fc5a215bd29874a67298867f3e17b8a39639b
ishimwezachee <zacheeishimwe@gmail.com>
2022-04-21 16:26:35 +0200

    add greetings functionality

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -3,7 +3,7 @@
 // of the page.
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
